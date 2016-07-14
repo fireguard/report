51de05dfb8f6a89a4907cdf67b4202eb6cb8d24f
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2016-07-14 20:42:04 -0300

    Criando método genérico para compressão de strings

--- src/Report/Exporters/Exporter.php
+++ src/Report/Exporters/Exporter.php
@@ -127,6 +127,15 @@ abstract class Exporter implements ExporterContract
         return $this;
     }
 
+    public function compress($buffer)
+    {
+        // remove comments
+        $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+        // remove tabs, spaces, newlines, etc.
+        $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '   ', '    '), '', $buffer);
+        return $buffer;
+    }
+
     /**
      * @return void
      */
